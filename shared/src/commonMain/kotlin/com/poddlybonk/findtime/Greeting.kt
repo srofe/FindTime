c9239d5b28653987c852275b05aa90c08a6f9a85
Simon Rofe <simon.rofe@ieee.org>
2022-04-26 12:47:49 +1000

    Initial commit of new Multiplatform Project`

---
+++ shared/src/commonMain/kotlin/com/poddlybonk/findtime/Greeting.kt
@@ -0,0 +1,7 @@
+package com.poddlybonk.findtime
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/poddlybonk/findtime/Platform.kt
@@ -0,0 +1,5 @@
+package com.poddlybonk.findtime
+
+expect class Platform() {
+    val platform: String
+}
