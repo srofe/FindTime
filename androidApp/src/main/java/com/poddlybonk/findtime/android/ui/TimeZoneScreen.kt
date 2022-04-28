4390d1ebf1c0060697378fdd516c9629897078e3
Simon Rofe <simon.rofe@ieee.org>
2022-04-28 18:39:23 +1000

    Add content to TimeZoneScreen

--- androidApp/src/main/java/com/poddlybonk/findtime/android/ui/TimeZoneScreen.kt
+++ androidApp/src/main/java/com/poddlybonk/findtime/android/ui/TimeZoneScreen.kt
@@ -1,13 +1,16 @@
 package com.poddlybonk.findtime.android.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import com.poddlybonk.findtime.TimeZoneHelper
 import com.poddlybonk.findtime.TimeZoneHelperImpl
+import kotlinx.coroutines.delay
 
 const val timeMillis = 1000 * 60L
 
@@ -16,6 +19,17 @@ fun TimeZoneScreen(currentTimeZoneStrings: SnapshotStateList<String>) {
     val timezoneHelper: TimeZoneHelper = TimeZoneHelperImpl()
     val listState = rememberLazyListState()
     Column(modifier = Modifier.fillMaxWidth()) {
-        // TODO: Add content
+        var time by remember { mutableStateOf(timezoneHelper.currentTime()) }
+        LaunchedEffect(0) {
+            while (true) {
+                time = timezoneHelper.currentTime()
+                delay(timeMillis)
+            }
+        }
+        LocalTimeCard(
+            city = timezoneHelper.currentTime(),
+            time = time,
+            date = timezoneHelper.getDate(timezoneHelper.currentTimeZone()))
+        Spacer(modifier = Modifier.size(16.dp))
     }
 }
