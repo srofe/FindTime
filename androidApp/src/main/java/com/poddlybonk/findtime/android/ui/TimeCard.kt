d764d7bc9b31ec7da9770f6684fed13988096e63
Simon Rofe <simon.rofe@ieee.org>
2022-04-28 19:46:07 +1000

    Add a TimeCard content

--- androidApp/src/main/java/com/poddlybonk/findtime/android/ui/TimeCard.kt
+++ androidApp/src/main/java/com/poddlybonk/findtime/android/ui/TimeCard.kt
@@ -5,16 +5,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.poddlybonk.findtime.android.theme.AppTheme
 
 @Composable
 fun TimeCard(timezone: String, hours: Double, time: String, date: String) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
+//            .fillMaxSize()
             .height(120.dp)
             .background(Color.White)
             .padding(8.dp)
@@ -26,7 +33,71 @@ fun TimeCard(timezone: String, hours: Double, time: String, date: String) {
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            // TODO: Add content
+            Box(modifier = Modifier
+                .background(color = Color.White)
+                .padding(16.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column(horizontalAlignment = Alignment.Start) {
+                        Text(
+                            timezone, style = TextStyle(
+                                color = Color.Black,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
+                            )
+                        )
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Row {
+                           Text(
+                               hours.toString(),
+                               style = TextStyle(
+                                   color = Color.Black,
+                                   fontWeight = FontWeight.Bold,
+                                   fontSize = 14.sp
+                               )
+                           )
+                            Text(
+                                " hours from local",
+                                style = TextStyle(
+                                    color = Color.Black,
+                                    fontSize = 14.sp
+                                )
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.weight(1.0f))
+                    Column(horizontalAlignment = Alignment.End) {
+                        Text(
+                            time,
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 24.sp
+                            )
+                        )
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Text(
+                            date,
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 12.sp
+                            )
+                        )
+                    }
+                }
+            }
         }
     }
 }
+
+@Preview
+@Composable
+fun TimeCardPreview() {
+    AppTheme {
+        TimeCard(timezone = "Australia/Perth", hours = 4.0, time = "5:00 am", date = "Friday, 29 May")
+    }
+}
+
